54026e17e8c6fb7ba55c084187f8ad5cdda69d8d
Biswajit Das <workbiswajit08981@gmail.com>
2025-03-26 12:57:05 +0530

    update form input

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -6,14 +6,14 @@ import { cn } from "../utils/style";
 const inputVariants = cva("w-full relative outline-none text-dark-txt font-base", {
   variants: {
     variant: {
-      default: "border border-gray-300 rounded-md bg-gray-700",
+      default: "border border-gray-300 rounded-md bg-gray-700 bg-transparent",
       outline: "border border-gray-400 bg-transparent focus:border-blue-500",
       border_b: "border-b-2 border-gray-500 bg-gray-700 rounded-xl",
       border_md: "border-2 border-gray-700 focus:border-blue-500 rounded-md",
     },
     inputSize: { // Renamed from 'size' to 'inputSize' to avoid conflict
       big: "h-12 text-lg px-4",
-      medium: "h-10 text-md px-3",
+      medium: "h-10 text-[18px] px-3",
       sm: "h-8 text-sm px-2",
     },
   },
