28bb983309f0d2a7934271f836acc279f9b711a3
Biswajit Das <workbiswajit08981@gmail.com>
2025-03-26 17:10:27 +0530

    modify auth header

--- src/components/AuthHeader.tsx
+++ src/components/AuthHeader.tsx
@@ -9,8 +9,8 @@ const AuthHeader = ({
 }) => {
   return (
     <div className="w-full relative overflow-hidden">
-      <div className="w-full relative overflow-hidden max-h-[12rem]  mt-[30%] text-left">
-        <h1 className="text-super">{text1}</h1>
+      <div className="w-full relative overflow-hidden max-h-[12rem]  mt-[30%] text-left pl-2  md:mt-[20%] lg:mt-[10%]">
+        <h1 className="text-super ">{text1}</h1>
         <h1 className="text-super">{text2}</h1>
         <h3 className="text-max text-gray-600">{text3}</h3>
       </div>
