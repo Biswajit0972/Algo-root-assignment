90226c9fdef4643a30a8753367e681b205c3c29a
Biswajit Das <workbiswajit08981@gmail.com>
2025-03-26 17:11:05 +0530

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import { AuthLayout, Dashboard, Details, Home, Signin, Signup } from "./utils";
 
 const App = () => {
   return (
-    <div className="h-dvh w-full relative bg-dark">
-     
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Signin />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
 
-export default App
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<Home />} />
+          <Route path="details" element={<Details />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
