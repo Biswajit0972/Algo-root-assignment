c0ce54c4e44f06a4ff296e239c34fc092d0c8d91
Biswajit Das <workbiswajit08981@gmail.com>
2025-03-26 17:10:57 +0530

    update authlayout

--- src/layouts/Auth/AuthLayout.tsx
+++ src/layouts/Auth/AuthLayout.tsx
@@ -1,21 +1,13 @@
-import { Outlet } from "react-router"
-
+import { Outlet } from "react-router";
 
 const AuthLayout = () => {
   return (
-    <div className="authLayout font-base bg-dark text-secondary">
-        <div className="w-full relative overflow-hidden">
-            <div className="w-full relative overflow-hidden max-h-[12rem]  mt-[30%] text-left">
-                <h1 className="text-super">Welcome</h1>
-                <h1 className="text-super">Back</h1>
-                <h3 className="text-max text-gray-600">Sign in to continue</h3>
-            </div>
-            <div className="w-full relative overflow-hidden mt-12">
-                <Outlet/>
-            </div>
-        </div>
+    <div className="authLayout font-base bg-dark text-secondary flex-center">
+     <div className="w-full h-full relative md:w-[80%] lg:w-1/2">
+     <Outlet />
+     </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
