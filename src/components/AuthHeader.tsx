8c6097be60b2dc73392cb07ab36e5046e04b3228
Biswajit Das <workbiswajit08981@gmail.com>
2025-03-26 13:04:05 +0530

    Add Auth header

---
+++ src/components/AuthHeader.tsx
@@ -0,0 +1,21 @@
+const AuthHeader = ({
+  text1,
+  text2,
+  text3,
+}: {
+  text1: string;
+  text2: string;
+  text3: string;
+}) => {
+  return (
+    <div className="w-full relative overflow-hidden">
+      <div className="w-full relative overflow-hidden max-h-[12rem]  mt-[30%] text-left">
+        <h1 className="text-super">{text1}</h1>
+        <h1 className="text-super">{text2}</h1>
+        <h3 className="text-max text-gray-600">{text3}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default AuthHeader;
