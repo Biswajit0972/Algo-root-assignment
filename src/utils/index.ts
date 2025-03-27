76a20c5db94c1f5a54a4a220fab606790bcd59c0
Biswajit Das <workbiswajit08981@gmail.com>
2025-03-27 09:15:08 +0530

    update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import AuthLayout from "../layouts/Auth/AuthLayout";
-import HomeLayout from "../layouts/home/HomeLayout";
+
 import Dashboard from "../layouts/home/pages/Dashboard";
 import Signin from "../layouts/Auth/pages/Signin";
 import Signup from "../layouts/Auth/pages/Signup";
@@ -8,4 +8,6 @@ import Home from "../layouts/home/pages/Home";
 import Details from "../layouts/home/pages/Details";
 
 
-export {AuthLayout, HomeLayout, Dashboard, Signin, Signup, Home, Details}
+export {AuthLayout, Dashboard, Signin, Signup, Home, Details};
+
+
