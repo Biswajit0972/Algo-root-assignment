6c2c90736e418f16338f6036a66f91eaf919bdf7
Biswajit Das <workbiswajit08981@gmail.com>
2025-03-28 17:54:33 +0530

    fix

--- src/components/AuthHeader.tsx
+++ src/components/AuthHeader.tsx
@@ -1,3 +1,5 @@
+
+
 const AuthHeader = ({
   text1,
   text2,
@@ -9,6 +11,7 @@ const AuthHeader = ({
 }) => {
   return (
     <div className="w-full relative overflow-hidden">
+     
       <div className="w-full relative overflow-hidden max-h-[12rem]  mt-[30%] text-left pl-2  md:mt-[20%] lg:mt-[10%]">
         <h1 className="text-super ">{text1}</h1>
         <h1 className="text-super">{text2}</h1>
--- src/layouts/Auth/AuthLayout.tsx
+++ src/layouts/Auth/AuthLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
 
 
+
 const AuthLayout = () => {
   return (
     <div className="authLayout font-base bg-dark text-secondary flex-center">
+      
       <div className="w-full h-full relative md:w-[80%] lg:w-1/2">
         <Outlet />
       </div>
